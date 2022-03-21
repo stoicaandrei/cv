8611a4e4da1e768bbefe0903eeeec61e6b1c5298
Andrei Stoica <andrei00stoica@gmail.com>
2022-03-21 19:28:35 +0200

    Create cv page

---
+++ src/pages/cv.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+const CVPage: NextPage = () => {
+  return <p>Here will be the cv</p>;
+};
+
+export default CVPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from 'next';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
-  return <div>Hello</div>;
+  return <Link href="/cv">You can see my CV here</Link>;
 };
 
 export default Home;
