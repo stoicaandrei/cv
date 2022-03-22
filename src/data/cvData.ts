27eefc5e9ec37cc129c9d1121aa1884bd9e9dcd7
Andrei Stoica <andrei00stoica@gmail.com>
2022-03-22 16:28:48 +0200

    Make relevant elements bold

--- src/data/cvData.ts
+++ src/data/cvData.ts
@@ -41,8 +41,8 @@ const data: CVData = {
       description: `
 <p>The client, an e-learning startup dedicated to helping students with their university admission process, had problems scaling up the business and was unable to obtain more funding from investors, due to lacking a centralized system for content, staff and stats.</p>
 <ul class="list-disc pl-5">
-<li>Secured funding of over $500k by building from scratch the MVP of a new centralized system by working closely with the client and understanding their needs. Also, fixed 90% of bugs by fixing the flaws in the existing system architecture.</li>
-<li>Increased overall review scores by more than 150% and customer satisfaction by over 40% by improving user feedback process and solving their existing problems.</li>
+<li><b>Secured funding of over $500k by building from scratch the MVP of a new centralized system</b> by working closely with the client and understanding their needs. Also, fixed 90% of bugs by fixing the flaws in the existing system architecture.</li>
+<li><b>Increased overall review scores by more than 150% and customer satisfaction by over 40%</b> by improving user feedback process and solving their existing problems.</li>
 </ul>
 <p>Skills: Nodejs, Express, MongoDB, Reactjs, React Query, Ant Design, NX monorepo, Digitalocean CI/CD, System Architecture, Agile, Asana, Pandadoc, Pubnub, socket.io</p>
       `,
@@ -55,8 +55,8 @@ const data: CVData = {
       endDate: '2021-08-01',
       description: `<p>The client, a startup providing e-learning services for the fintech sector, was struggling with low student satisfaction and low productivity among trainers, due to lacking a controlled environment for students to practice portfolio management.</p>
 <ul class="list-disc pl-5">
-<li>Built a successful MVP in less than 5 months by keeping a starling line of communication with the client and by efficiently managing a team to optimize all client needs inside the web app.</li>
-<li>Increased company profit by over 60% and client's customer pool by more than 25% by continuously growing the product over the next year and by constantly releasing new, highly requested features.</li>
+<li><b>Built a successful MVP in less than 5 months</b> by keeping a starling line of communication with the client and by efficiently managing a team to optimize all client needs inside the web app.</li>
+<li><b>Increased company profit by over 60% and client's customer pool by more than 25%</b> by continuously growing the product over the next year and by constantly releasing new, highly requested features.</li>
 </ul>
 <p>Skills: Django, PostgreSQL, Redis, Channels, Celery, Reactjs, Redux, Ant Design, AWS CI/CD, System Architecture, Agile Team Management, Jira</p>`,
     },
@@ -68,8 +68,8 @@ const data: CVData = {
       endDate: '2021-02-01',
       description: `<p>The clients, a large group of Romanian high school students, were wasting money on private lessons and had no other efficient alternative when trying to study due to lacking a platform for finding reliable information tailored to their needs.</p>
 <ul class="list-disc pl-5">
-<li>Succeeded in pitching the project to over 10 media apparitions and talked to investors by taking part in a start-up accelerator by developing from scratch a fully tailored MVP in less than 4 months.</li>
-<li>Achieved over 5K users in the app and website, with over 6K hours of learning tracked in the first year after the launch by consistently offering users sought after features and continuously improving the product.</li>
+<li><b>Succeeded in pitching the project to over 10 media apparitions and talked to investors</b> by taking part in a start-up accelerator by developing from scratch a fully tailored MVP in less than 4 months.</li>
+<li><b>Achieved over 5K users in the app and website, with over 6K hours of learning tracked in the first year after the launch</b> by consistently offering users sought after features and continuously improving the product.</li>
 </ul>
 <p>Skills: Nodejs, Express, MongoDB, jQuery, pug.js, LaTex, MathJax, Digitalocean, Team Management, Trello, Start-up Development, Git</p>`,
     },
