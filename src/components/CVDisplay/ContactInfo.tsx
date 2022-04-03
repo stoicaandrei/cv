384183036b9d3e365ed08f6475b350fa82ec4ed6
Andrei Stoica <andrei00stoica@gmail.com>
2022-04-03 22:46:25 +0300

    Fix key duplication warning

--- src/components/CVDisplay/ContactInfo.tsx
+++ src/components/CVDisplay/ContactInfo.tsx
@@ -10,11 +10,11 @@ type Props = {
 const ContactInfo = ({ items }: Props) => {
   return (
     <div className="grid grid-cols-[min-content,1fr] gap-y-1 gap-x-2 ">
-      {items.map((item) => {
+      {items.map((item, index) => {
         const icon = item.icon.split(' ') as unknown as [IconPrefix, IconName];
 
         return (
-          <Fragment key={item.text}>
+          <Fragment key={index}>
             <FontAwesomeIcon
               icon={icon}
               className="self-center justify-self-center"
