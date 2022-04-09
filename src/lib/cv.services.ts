7f1e4c2ec579177a4f66ea3142d8d9153443721b
Andrei Stoica <andrei00stoica@gmail.com>
2022-04-09 19:52:12 +0300

    Improve cv.services

--- src/lib/cv.services.ts
+++ src/lib/cv.services.ts
@@ -1,8 +1,9 @@
 import { doc, getDoc } from 'firebase/firestore';
 import { cvCollection } from './firebase/firestore.services';
 
+export const cvDoc = (slug: string) => doc(cvCollection, slug);
 export const getCv = async (slug: string) => {
-  const ref = doc(cvCollection, slug);
+  const ref = cvDoc(slug);
 
   return await getDoc(ref);
 };
