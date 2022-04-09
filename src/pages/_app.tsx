89007bb150e96c5540b2b62b6f514a0339f6a0e8
Andrei Stoica <andrei00stoica@gmail.com>
2022-04-09 23:13:36 +0300

    Improve styling in edit cv page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,7 @@ import type { AppProps } from 'next/app';
 import 'lib/firebase';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <div className="min-w-screen min-h-screen bg-gray-100">
-      <Component {...pageProps} />
-    </div>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- src/pages/admin/edit-cv.tsx
+++ src/pages/admin/edit-cv.tsx
@@ -4,7 +4,11 @@ import { useAuthCheck } from 'hooks';
 const EditCVPage: NextPage = () => {
   useAuthCheck();
 
-  return <p>Here you edit the cv</p>;
+  return (
+    <div className="m-5 rounded  bg-white p-5 shadow-md">
+      <h1 className="text-xl font-bold">Edit CV</h1>
+    </div>
+  );
 };
 
 export default EditCVPage;
