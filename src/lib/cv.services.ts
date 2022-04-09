49f0793a1a67625258c7c24ecfae23f2f54ffee1
Andrei Stoica <andrei00stoica@gmail.com>
2022-04-09 23:26:40 +0300

    Define function to update cv

--- src/lib/cv.services.ts
+++ src/lib/cv.services.ts
@@ -1,9 +1,16 @@
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { cvCollection } from './firebase/firestore.services';
+import { CVData } from 'types';
 
 export const cvDoc = (slug: string) => doc(cvCollection, slug);
 export const getCv = async (slug: string) => {
   const ref = cvDoc(slug);
 
   return await getDoc(ref);
+};
+
+export const updateCV = async (slug: string, data: Partial<CVData>) => {
+  const ref = cvDoc(slug);
+
+  return await updateDoc(ref, data);
 };
