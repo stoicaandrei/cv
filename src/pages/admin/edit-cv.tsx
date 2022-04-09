7b5581b829f72c90e8e767dc78ffe291ab318d6d
Andrei Stoica <andrei00stoica@gmail.com>
2022-04-09 23:37:09 +0300

    Integrate form hook in editCv

--- src/pages/admin/edit-cv.tsx
+++ src/pages/admin/edit-cv.tsx
@@ -1,7 +1,7 @@
 import { GetStaticProps, NextPage } from 'next';
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { useAuthCheck } from 'hooks';
-import { getCv } from 'lib';
+import { getCv, updateCV } from 'lib';
 import { CVData } from 'types';
 
 type Props = {
@@ -11,21 +11,41 @@ type Props = {
 const EditCVPage: NextPage<Props> = ({ data }) => {
   useAuthCheck();
 
+  const formData = {
+    name: data.name,
+    title: data.title,
+    photoUrl: data.photoUrl,
+  };
+
+  const onSubmit = (data: typeof formData) => {
+    updateCV('test', data);
+  };
+
+  const { register, handleSubmit } = useForm({ defaultValues: formData });
+
   return (
     <div className="m-5 rounded  bg-white p-5 shadow-md">
       <div className="flex flex-row items-center justify-between">
         <h1 className="text-xl font-bold">Edit CV</h1>
-        <button className="rounded bg-blue-400 px-5 py-2 font-semibold text-white">
+        <button
+          onClick={handleSubmit(onSubmit)}
+          className="rounded bg-blue-400 px-5 py-2 font-semibold text-white"
+        >
           Save
         </button>
       </div>
-      <input type="text" value={data?.title} />
+      <form>
+        <div className="flex flex-col gap-3 pt-6">
+          <input {...register('name')} type="text" />
+          <input {...register('title')} type="text" />
+        </div>
+      </form>
     </div>
   );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const cv = await getCv('javascript');
+  const cv = await getCv('test');
 
   return {
     props: {
