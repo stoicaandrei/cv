6f7849867f17b0d4ed90cae7c10eb2ea70dd75b5
Andrei Stoica <andrei00stoica@gmail.com>
2022-04-10 20:49:15 +0300

    Test react beautiful dnd

--- src/components/CVForm/index.tsx
+++ src/components/CVForm/index.tsx
@@ -1,6 +1,9 @@
+import dynamic from 'next/dynamic';
 import { UseFormReturn } from 'react-hook-form';
 import { Input, Label } from 'components/ui';
 import { CVData } from 'types';
+
+const DndTest = dynamic(() => import('./DnDTest'), { ssr: false });
 
 type Props = {
   form: UseFormReturn<CVData>;
@@ -8,7 +11,7 @@ type Props = {
 };
 
 const CVForm = ({ onSubmit, form }: Props) => {
-  const { register, handleSubmit } = form;
+  const { register, handleSubmit, getValues } = form;
 
   return (
     <form id="cv-edit-form" onSubmit={handleSubmit(onSubmit)}>
@@ -17,6 +20,7 @@ const CVForm = ({ onSubmit, form }: Props) => {
         <Input {...register('name')} className="mb-3" />
         <Label htmlFor="title" name="Position Title" />
         <Input {...register('title')} className="mb-3" />
+        <DndTest items={getValues().sidebar} />
       </div>
     </form>
   );
