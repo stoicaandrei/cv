970ea80d739525f4441cdcee2b4abb7a2b7fe6ad
Andrei Stoica <andrei00stoica@gmail.com>
2022-07-01 21:27:28 +0300

    copy cv display page in cv edit page

--- src/pages/admin/cv/[slug].tsx
+++ src/pages/admin/cv/[slug].tsx
@@ -1,7 +1,44 @@
-import { NextPage } from 'next';
+import { GetServerSideProps, NextPage } from 'next';
+import Head from 'next/head';
+import { useState } from 'react';
+import { CVDisplay } from 'components';
+import { getCv } from 'lib';
+import { CVData } from 'types';
 
-const AdminCVEditPage: NextPage = () => {
-  return null;
+type Props = {
+  data: CVData;
+};
+
+const AdminCVEditPage: NextPage<Props> = (props) => {
+  const [short, setShort] = useState(true);
+
+  return (
+    <div>
+      <Head>
+        <title>stoica.dev | CV</title>
+      </Head>
+      <div className="print:hidden">
+        <input
+          type="checkbox"
+          checked={short}
+          onChange={(e) => setShort(e.target.checked)}
+        />
+        Display short version
+      </div>
+      <CVDisplay data={props.data} short={short} />
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const slug = context.query.slug as string;
+  const cv = await getCv(slug);
+
+  return {
+    props: {
+      data: cv.data(),
+    },
+  };
 };
 
 export default AdminCVEditPage;
