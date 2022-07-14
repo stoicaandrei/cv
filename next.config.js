1bd053cc4d5986efcbf6c2d613f84699ad706fbb
Andrei Stoica <andrei00stoica@gmail.com>
2022-07-14 22:32:48 +0300

    Add openings redirects

--- next.config.js
+++ next.config.js
@@ -4,6 +4,25 @@ const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/openings/15min',
+        destination: 'https://fantastical.app/andrei00stoica-OGIi/15-min-call',
+        permanent: true,
+      },
+      {
+        source: '/openings/30min',
+        destination: 'https://fantastical.app/andrei00stoica-OGIi/30-min-call',
+        permanent: true,
+      },
+      {
+        source: '/openings/1hr',
+        destination: 'https://fantastical.app/andrei00stoica-OGIi/1-hour-call',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
